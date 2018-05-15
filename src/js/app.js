9c2c2c1f5ec533181140b8fe57622cdf3cd80165
IreneSalomo <irene.salomo@gmail.com>
2018-05-15 12:14:56 +1000

    simplify exercise, put data in function instead of object

--- src/js/app.js
+++ src/js/app.js
@@ -1,56 +1,59 @@
-Vue.component('the-breakfast', {
-    template: '<div class="thepulse-breakfast--icon" \
-                ><img v-bind:src="name + \'.png\'"> \
-                <div class="thepulse-breakfast--icon--name" v-on:click="$emit(\'click-breakfast-item\')"> \
-                {{ name }}</div> \
-                </div>',
-    props: ['name'],
-    methods: {
-        selectBreakfastItem: function(){
-          console.log("test");
-          console.log(this.key);
-        }
-      }                  
-  })
+// Vue.component('the-breakfast', {
+//     template: '<div class="thepulse-breakfast--icon" \
+//                 ><img v-bind:src="name + \'.png\'"> \
+//                 <div class="thepulse-breakfast--icon--name" v-on:click="$emit(\'click-breakfast-item\')"> \
+//                 {{ name }}</div> \
+//                 </div>',
+//     props: ['name'],
+//     methods: {
+//         selectBreakfastItem: function(){
+//           console.log("test");
+//           console.log(this.key);
+//         }
+//       }                  
+//   })
 var vmBreakfast = new Vue({
     el: '#component-breakfast', 
-    data: {
-        breakfastItems: [
-            {
-                id: 1,
-                name: 'Cereal',
-            },
-            {
-                id: 2,
-                name: 'Oats',
-            },
-            {
-                id: 3,
-                name: 'Fruits',
-            },
-            {
-                id: 4,
-                name: 'Toast',
-            },
-            {
-                id: 5,
-                name: 'Bacon',
-            },
-            {
-                id: 6,
-                name: 'Smoothies',
-            },
-        ]
+    data() {
+        return {
+            breakfastItems: [
+                {
+                    id: 1,
+                    name: 'Cereal',
+                },
+                {
+                    id: 2,
+                    name: 'Oats',
+                },
+                {
+                    id: 3,
+                    name: 'Fruits',
+                },
+                {
+                    id: 4,
+                    name: 'Toast',
+                },
+                {
+                    id: 5,
+                    name: 'Bacon',
+                },
+                {
+                    id: 6,
+                    name: 'Smoothies',
+                },
+            ],
+            myBreakfastName : "Your favourite breakfast item:",
+        }
     },
     
 
 });
 
-var target1 = document.querySelector('.thepulse-breakfast--icon:nth-of-type(1) .thepulse-breakfast--icon--name');
-    console.log(target1);
-    Velocity(target1, 
-      { translateY: '70%'},
-      { easing: [1000, 20], 
-        duration: 2000 
-      }
-    );
+// var target1 = document.querySelector('.thepulse-breakfast--icon:nth-of-type(1) .thepulse-breakfast--icon--name');
+//     console.log(target1);
+//     Velocity(target1, 
+//       { translateY: '70%'},
+//       { easing: [1000, 20], 
+//         duration: 2000 
+//       }
+//     );
